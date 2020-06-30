b362ead5a71e0b97786f9d2308a5d87452a1f15f
Ubuntu <ubuntu@ip-172-31-24-11.us-east-2.compute.internal>
2020-06-30 13:55:48 +0000

    set up login function usinf session hash.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
     
     def create
+        @user = User.new(params.require(:user).permit(:name))
+        if @user.save
+            flash[:notice] = "Welcome #{@user.name}"
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
     end
     
     def show
