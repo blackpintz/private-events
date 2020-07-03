f95f56f3b048f255b0c068c8e2080b22f12e5528
Ubuntu <ubuntu@ip-172-31-24-11.us-east-2.compute.internal>
2020-07-03 13:14:15 +0000

    Fix rubocop warnings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
-    
-    def create
-        @user = User.new(params.require(:user).permit(:name))
-         @user.save
-            session[:user_id] = @user.id
-            flash[:notice] = "Welcome #{@user.name}"
-            redirect_to user_path(@user)
-    end
-    
-    def show
-        @user = User.find(params[:id])
-        @created_events = @user.events
-        @events = @user.attended_events
-        @upcoming_events = @created_events.future(Date.today)
-        @previous_events = @created_events.past(Date.today)
-    end
-    
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name))
+    @user.save
+    session[:user_id] = @user.id
+    flash[:notice] = "Welcome #{@user.name}"
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @created_events = @user.events
+    @events = @user.attended_events
+    @upcoming_events = @created_events.future(Date.today)
+    @previous_events = @created_events.past(Date.today)
+  end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,27 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-     let(:user1) {FactoryBot.create(:user)}
-     
-     describe "GET #new" do
-         it "renders new template" do
-             get :new
-             expect(response).to render_template(:new)
-         end
-     end
-     
-     describe "POST #create" do
-         it "redirects to users show page" do
-              post :create, params: {user: {name: "Rose"}}
-              expect(response).to redirect_to(user_url(User.find_by(name: "Rose")))
-         end
-     end
-     
-     describe "GET #show" do
-          it "renders show template" do
-               get :show, params: {id: 1}
-               expect(response).to render_template(:show)
-          end
-     end
-    
+  let(:user1) { FactoryBot.create(:user) }
+
+  describe 'GET #new' do
+    it 'renders new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'redirects to users show page' do
+      post :create, params: { user: { name: 'Rose' } }
+      expect(response).to redirect_to(user_url(User.find_by(name: 'Rose')))
+    end
+  end
+
+  describe 'GET #show' do
+    it 'renders show template' do
+      get :show, params: { id: 1 }
+      expect(response).to render_template(:show)
+    end
+  end
 end
